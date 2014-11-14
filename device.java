0bef2b82f89213409498cc9f911d5f1f786ac9e0
yangac <ayang2007@gmail.com>
2014-11-13 22:47:16 -0800

    Added files to parse text files for DB entry

---
+++ device.java
@@ -0,0 +1,15 @@
+//Project Stark File parser
+public class device 
+{
+	String make;
+	String model;
+	String GSMBands;
+	String LTEBands;
+	String UMTSBands;
+	
+	public device() 
+	{
+		// TODO Auto-generated constructor stub
+	}
+
+}
